c6ea5ddab81c0a58fc85cf24d6b09cb33dd06467
Emily_Pereira <Emily07ribeiro@gmail.com>
2025-03-25 20:40:25 -0300

    exercicio 3

---
+++ Exercicios/assunto2_Exercicio6 copy/src/aplicacao/mediaIdades.java
@@ -0,0 +1,34 @@
+package aplicacao;
+
+import java.util.Scanner;
+
+public class mediaIdades {
+    public static void main(String[] args) {
+        Scanner le = new Scanner(System.in);
+
+        final int N = 5;
+        int idades[] = new int[N];
+        int soma = 0;
+        double media;
+
+        
+        for (int i = 0; i < N; i++) {
+            System.out.print("Idade? ");
+            idades[i] = le.nextInt();
+            soma += idades[i]; 
+        }
+
+        
+        media = (double) soma / N; 
+        System.out.println("A média de idades: " + media);
+
+        System.out.println("\nIdades acima da média:");
+        for (int i = 0; i < N; i++) { 
+            if (idades[i] > media) {
+                System.out.println("Idade: " + idades[i]); 
+            }
+        }
+
+        le.close();
+    }
+}
